c7a7e907b431ab87cc2093ba1070ddc4678f66d8
Dick Hardt <dick.hardt@gmail.com>
2023-09-18 08:44:35 -0700

    remove notes

--- utils/src/index.ts
+++ utils/src/index.ts
@@ -1,32 +1,4 @@
-
-
-// validScopes - returns array of valid scopes
-
-// isValidScope - checks if scope is valid
-
-// pkce
-//  returns { code_verifier, code_challenge }
-
-// verifyChallenge ( code_verifier, expectedChallenge )
-
-// generateChallenge ( code_verifier )
-// returns a code challenge
-
-// createAuthRequest
-// required 
-//      client_id
-// optional
-//      scope
-//      response_type - id_token (default  code)
-//      response_mode - fragment query (default form_post)
-//      endpoint
-//      nonce
-//      state
-//      provider_hint
-//      
-// returns 
-//      { url, nonce, code_verifier }
-// 
+// wrapper for createAuthRequest and pkce modules
 
 export { default as createAuthRequest } from './createAuthRequest.js'
 export * from './createAuthRequest.js'
