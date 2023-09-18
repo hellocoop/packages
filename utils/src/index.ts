0e8d841a696a2508fb96703b1bcea536804fa9b4
Dick Hardt <dick.hardt@gmail.com>
2023-09-18 11:27:12 -0700

    Revert "attempt to fix .js appending"
    This reverts commit 1152511cb3eb68b6e99ee4cdc6b50a5d44747572.

--- utils/src/index.ts
+++ utils/src/index.ts
@@ -1,6 +1,6 @@
 // wrapper for createAuthRequest and pkce modules
 
-export { default as createAuthRequest } from './createAuthRequest'
-export * from './createAuthRequest'
-export { default as pkce } from './pkce'
-export * from './pkce'
+export { default as createAuthRequest } from './createAuthRequest.js'
+export * from './createAuthRequest.js'
+export { default as pkce } from './pkce.js'
+export * from './pkce.js'
