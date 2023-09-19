3a761f5401fe965148413f0d57927c56f979e0e2
rohanharikr <rohanharikumar80@gmail.com>
2023-09-19 22:51:49 +0530

    move current profile page to home

--- test-app/app/page.tsx
+++ test-app/app/page.tsx
@@ -1,113 +1,60 @@
-import Image from 'next/image'
+import { useUser, UpdateProfileButton, SignInButton } from "@hellocoop/nextjs"
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const { user } = useUser()
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return (
+        <main className='max-w-4xl mx-auto p-4'>
+            <header className='flex justify-between items-center'>
+                <h1 className='text-2xl font-semibold'>Hellō Next.js Example App</h1>
+                {user?.isLoggedIn && <a href="/auth/hellocoop?logout=true" className='mt-1'>Log Out</a>}
+            </header>
+            <hr className='my-4'/>
+            {user?.isLoggedIn ? 
+                <section className='max-w-xl mx-auto p-4'>
+                    <h2 className='text-xl text-left px-4'>Your Profile</h2>
+                    <ul className='divide-y'>
+                        <li className='p-4 flex flex-col justify-center items-center'>
+                            {/* Use Image from next/image */}
+                            <img src={user.picture} alt={user.name} className='rounded-full h-32 w-32 object-cover mb-3 bg-gray-300'/>
+                            {/* TODO: Update UpdateProfileButton Component from hellocoop/nextjs library to accept label prop */}
+                            <UpdateProfileButton/>
+                        </li>
+                        <li className='p-4'>
+                            <div className='flex flex-col'>
+                                <span className='text-xs opacity-60 uppercase'>Name</span>
+                                <span className='text-lg'>{user.name}</span>
+                            </div>
+                        </li>
+                        <li className='p-4'>
+                            <div className='flex flex-col mb-2'>
+                                <span className='text-xs uppercase opacity-60'>Email</span>
+                                <span className='text-lg'>{user.email}</span>
+                            </div>
+                            {/* TODO: Update UpdateProfileButton Component from hellocoop/nextjs library to accept label prop */}
+                            <UpdateProfileButton/>
+                        </li>
+                        <li className='flex flex-col p-4'>
+                            <span className='text-xs uppercase opacity-60'>JSON Payload</span>
+                            <pre className='text-sm mt-2 font-mono'>{JSON.stringify(user, null, 2)}</pre>
+                        </li>
+                    </ul>
+                </section> : 
+                <section className='max-w-xl mx-auto p-4 flex flex-col items-center'>
+                    <h2 className='text-xl text-left px-4 font-semibold'>Lorem Ipsum</h2>
+                    <p className='text-center px-4 mt-4'>This example demonstrates how to set up user authentication with Hellō and session management with iron-session. It is based off the with-iron-session example, which uses a signed and encrypted cookie to store session data.</p>
+                    <ul className='mt-4 mb-8 flex space-x-2 '>
+                        <li>Name</li>
+                        <li>Email</li>
+                        <li>Picture</li>
+                    </ul>
+                    <SignInButton/>
+                    <div className='space-x-8 mt-12'>
+                        <a href="https://www.hello.coop" className='underline inline-flex items-center'>Learn more about Hellō <svg xmlns="http://www.w3.org/2000/svg" className="h-4 ml-1 mt-0.5 opacity-80" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg></a>
+                        <a href="https://www.hello.dev" className='underline inline-flex items-center'>Hellō Developer Docs<svg xmlns="http://www.w3.org/2000/svg" className="h-4 ml-1 mt-0.5 opacity-80" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg></a>
+                    </div>
+                </section>
+            }
+        </main>
+    )
 }
