20da228ec068985c143f3cd36c23a48d95a466db
Dick Hardt <dick.hardt@hello.coop>
2023-09-18 18:48:20 -0700

    hack crypto loading

--- utils/src/pkce.ts
+++ utils/src/pkce.ts
@@ -2,17 +2,19 @@
   derived from the solid work at https://github.com/crouchcd/pkce-challenge
 */
 
-import type { webcrypto } from 'node:crypto';
+import { webcrypto} from 'node:crypto';
 
 const VERIFIER_LENGTH = 43
 
-const crypto: webcrypto.Crypto =
-  globalThis.crypto?.webcrypto ?? // Node.js 16 REPL has globalThis.crypto as node:crypto
-  globalThis.crypto ?? // web browsers and Node.js 18+ 
-  (await import("node:crypto")).webcrypto; // Node.js 16 non-REPL
+// TODO - fix utils so can be used in browser
+//
+// const crypto: webcrypto.Crypto =
+//   globalThis.crypto?.webcrypto ?? // Node.js 16 REPL has globalThis.crypto as node:crypto
+//   globalThis.crypto ?? // web browsers and Node.js 18+ 
+//   (await import("node:crypto")).webcrypto; // Node.js 16 non-REPL
 
-const uuid4 = crypto.randomUUID
-export { uuid4 }
+
+export { randomUUID as uuidv4 } from 'node:crypto';
 
 /** Generate cryptographically strong random string
  * @param size The desired length of the string
@@ -22,7 +24,7 @@ function generateVerifier() {
   const mask =
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~";
   let result = "";
-  const randomUints = crypto.getRandomValues(new Uint8Array(VERIFIER_LENGTH));
+  const randomUints = webcrypto.getRandomValues(new Uint8Array(VERIFIER_LENGTH));
   for (let i = 0; i < VERIFIER_LENGTH; i++) {
     // cap the value of the randomIndex to mask.length - 1
     const randomIndex = randomUints[i] % mask.length;
@@ -36,7 +38,7 @@ function generateVerifier() {
  * @returns The base64 url encoded code challenge
  */
 export async function generateChallenge(code_verifier: string) {
-  const buffer = await crypto.subtle.digest(
+  const buffer = await webcrypto.subtle.digest(
     "SHA-256",
     new TextEncoder().encode(code_verifier)
   );
