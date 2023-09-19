27fa3eedfcfbd804441229fbd47a4e99db226387
Dick Hardt <dick.hardt@hello.coop>
2023-09-18 18:48:47 -0700

    add in test-app

---
+++ test-app/pages/api/hellocoop.js
@@ -0,0 +1,7 @@
+// export { handleAuth as default } from '../../../../../HelloCoop/packages/nextjs/dist/index.js'
+/* 
+import { setupAuth } from '@hellocoop/nextjs' 
+import localConfig from './localConfig.js'
+export default setupAuth(localConfig())
+*/
+export { handleAuth as default } from '@hellocoop/nextjs'
