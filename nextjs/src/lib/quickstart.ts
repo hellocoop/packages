0d78adb556f45c31a7c39d7c3dc806a5432ae44f
Dick Hardt <dick.hardt@hello.coop>
2023-09-21 16:51:43 -0700

    not used

--- nextjs/src/lib/quickstart.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from './config'
-
-export const buildQuickstartUrl = (config: Config) =>
-    new URL('https://quickstart.hello.coop/?' + new URLSearchParams({
-        ...((process.env.NODE_ENV === 'production' && config.quickstartRoute)
-            ? {}
-            : { response_uri: config.baseUrl + config.quickstartRoute }),
-        suffix: 'Next.js App'
-    }))
