47ce7563ae6337d7c2e2b1229be7ca46807fd20e
Dick Hardt <dick.hardt@gmail.com>
2023-09-20 21:00:24 -0700

    refactor session object

--- nextjs/src/lib/session.ts
+++ nextjs/src/lib/session.ts
@@ -2,11 +2,14 @@ import type { IronSessionOptions } from 'iron-session'
 
 import type { User } from './user'
 
+export type OIDC = {
+    nonce: string;
+    code_verifier: string
+}
 // type of req.session.*
 declare module 'iron-session' {
     interface IronSessionData {
         user?: User,
-        nonce?: string,
-        code_verifier?: string
+        oidc?: OIDC,
     }
 }
