e81e9e9b1e51ee1be8bfa3dae8a3cd7e9ad8f9d0
Dick Hardt <dick.hardt@hello.coop>
2023-09-21 16:50:20 -0700

    dynamically discover redirect_uri

--- nextjs/src/handlers/login.ts
+++ nextjs/src/handlers/login.ts
@@ -1,28 +1,43 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
 import { withIronSessionApiRoute } from 'iron-session/next'
-import { createAuthRequest } from '@hellocoop/utils'
+import { createAuthRequest, redirectURIBounce } from '@hellocoop/utils'
 import type { Config } from '../lib/config'
+
+var redirectURI:string | null = null // set dynamically if not configured
+
+var callCount = 0 // DEBUG
 
 const handleLoginFactory = (config: Config): NextApiHandler =>
     withIronSessionApiRoute(async (req: NextApiRequest, res: NextApiResponse) => {
-        const { target_uri, scope } = req.query
+        const { target_uri, scope, redirect_uri } = req.query
 
-        if (!config.baseUrl) {
-            res.status(500).end('Missing baseUrl configuration')
+        if (!config.clientId) {
+            res.status(500).end('Missing HELLOCOOP_CLIENT_ID configuration')
             return
         }
-        if (!config.helloClientId) {
-            res.status(500).end('Missing Hellō ClientId configuration')
-            return
+
+callCount++
+console.log('login called:',callCount)
+
+        if (!redirectURI) {
+            redirectURI = config?.redirectURI || 
+                process.env.HELLOCOOP_HOST ? process.env.HELLOCOOP_HOST + (process.env.HELLOCOOP_ROUTE || config.apiRoute) : redirect_uri as string
+            if (!redirectURI) {
+                console.log('Discovering API route ...')
+                return res.end(redirectURIBounce())    
+            }
+            console.log('We have now set redirectURI to:',redirectURI)
         }
+
         const { url, nonce, code_verifier } = await createAuthRequest({
-            redirect_uri: config.baseUrl,
-            client_id: config.helloClientId
+            redirect_uri: redirectURI,
+            client_id: config.clientId
             // TODO -- pass in scope from environment / config or passed in call
         })
         req.session.oidc = {
             nonce,
-            code_verifier
+            code_verifier,
+            redirect_uri: redirectURI
         }
 
         await req.session.save()
