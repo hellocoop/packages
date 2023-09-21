22e9485148704dea7c4ee229eb904713ce82c192
Dick Hardt <dick.hardt@gmail.com>
2023-09-20 21:01:23 -0700

    use Payload type for User

--- nextjs/src/lib/user.ts
+++ nextjs/src/lib/user.ts
@@ -1,26 +1,28 @@
 import { IronSessionData } from 'iron-session'
 
-export type HelloClaims = {
-    iss: string,
-    aud: string,
-    nonce: string,
-    jti: string,
-    sub: string,
-    scope: string[],
-    iat: number,
-    exp: number
-    name?: string,
-    nickname?: string,
-    picture?: string,
-    email?: string,
-    email_verified?: boolean,
-}
+// export type HelloClaims = {
+//     iss: string,
+//     aud: string,
+//     nonce: string,
+//     jti: string,
+//     sub: string,
+//     scope: string[],
+//     iat: number,
+//     exp: number
+//     name?: string,
+//     nickname?: string,
+//     picture?: string,
+//     email?: string,
+//     email_verified?: boolean,
+// }
+
+import { Payload } from '@hellocoop/utils'
 
 export type User = {
     isLoggedIn: false
 } | ({
     isLoggedIn: true
-} & HelloClaims)
+} & Payload)
 
 export interface GetUserOptions {
     session: IronSessionData
