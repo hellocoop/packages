0a5254a25d45e92c5013e70b3d3cd40ef6c5df43
Dick Hardt <dick.hardt@hello.coop>
2023-09-22 16:57:34 -0700

    start of quickstart-next.js

---
+++ quickstart-nextjs/index.mjs
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import qs from './quickstart-next.js';
+
+(async () => {
+  await qs();
+})();
---
+++ quickstart-nextjs/quickstart-next.js
@@ -0,0 +1,28 @@
+
+// import open from 'open'
+// import getPort from 'get-port'
+// import readline from 'readline'
+// import * as http from 'http'
+// import { URLSearchParams, parse } from 'url';
+// import page from './page.js'
+// import quickstart from '@hellocoop/quickstart'
+
+// import quickstart from '@hellocoop/nextjs'
+
+// const qs2 = require('@hellocoop/quickstart')
+
+
+const qs = async function () {   
+    // check if we are running on an interactive terminal
+    console.log('quickstart-next.js called')
+    // see if it looks like we are running in a next.js environment
+    // check if there is a next.config.js file -- if not, write one out
+    // load next.config.js and check if it has already been setup
+    // get client id
+    // generate secret
+    // update next.config.js
+}
+
+export default qs;
+
+
