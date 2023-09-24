4e7a3fec376ef9689a5720404819dbbe2eb7bf78
Dick Hardt <dick.hardt@gmail.com>
2023-09-24 15:57:43 -0700

    working commit

--- quickstart-nextjs/quickstart-next.js
+++ quickstart-nextjs/quickstart-next.js
@@ -1,26 +1,69 @@
 
-// import open from 'open'
-// import getPort from 'get-port'
-// import readline from 'readline'
-// import * as http from 'http'
-// import { URLSearchParams, parse } from 'url';
-// import page from './page.js'
-// import quickstart from '@hellocoop/quickstart'
 
-// import quickstart from '@hellocoop/nextjs'
-
-// const qs2 = require('@hellocoop/quickstart')
+import * as fs from 'fs'
+import { randomBytes } from 'crypto'
+import quickstart from '@hellocoop/quickstart'
 
 
 const qs = async function () {   
-    // check if we are running on an interactive terminal
-    console.log('quickstart-next.js called')
-    // see if it looks like we are running in a next.js environment
-    // check if there is a next.config.js file -- if not, write one out
-    // load next.config.js and check if it has already been setup
-    // get client id
-    // generate secret
-    // update next.config.js
+
+    console.log('Hellō Quickstart for Next.js ... ')
+
+    const nextConfigFile = process.cwd()+'/next.config.js'
+
+    // check if we are in a directory with next.config.js
+    try {
+        fs.accessSync(nextConfigFile,fs.constants.F_OK)
+    } catch(err){
+        const error = new Error('Could not find "next.config.js". Are we running in a Next.js project?')
+        console.error(error)
+        return error    
+    }
+
+    // make sure we can read and write
+    try {
+        fs.accessSync(nextConfigFile,fs.constants.R_OK | fs.constants.W_OK)
+    } catch(err){
+        const error = new Error('Do not have read and write access to "next.config.js".')
+        console.error(error)
+        return error    
+    }
+
+    // check that the values have not already been set
+    try {
+        const nextConfig = await import(nextConfigFile)
+        const client_id = nextConfig?.default?.env?.HELLOCOOP_CLIENT_ID_DEFAULT
+        if (client_id) {
+            console.log(`HELLOCOOP_CLIENT_ID_DEFAULT already configured as ${client_id}`)
+            return
+        }
+        console.log(JSON.stringify(nextConfig,null,4))
+    } catch(err) {
+        console.error('Error importing the package:', error);
+        return error 
+    }
+
+    const client_id = await quickstart() // TODO set some defaults
+
+    const session_secret = randomBytes(32).toString('hex')
+    const helloConfig = `
+        // added by @hellocoop/quickstart-nextjs
+        nextConfig.env = {
+            ...nextConfig.env,
+            HELLOCOOP_CLIENT_ID_DEFAULT: '${client_id}',
+            HELLOCOOP_SESSION_SECRET_DEFAULT: '${session_secret}'
+        }
+    `
+    try {
+        const err = fs.appendFileSync(nextConfigFile,helloConfig)
+    } catch(err) {
+        console.err(err)
+        return err
+    }
+    console.log('Updated next.config.js with:')
+    console.log(`HELLOCOOP_CLIENT_ID_DEFAULT:'${client_id}'`)
+    console.log(`HELLOCOOP_SESSION_SECRET_DEFAULT: '${session_secret}'`)
+    return
 }
 
 export default qs;
