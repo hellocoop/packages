38d2eec6f9660b5593fb155106d7707bfb05a09a
Dick Hardt <dick.hardt@gmail.com>
2023-09-28 15:38:27 -0700

    logout to set value

--- nextjs/src/handlers/logout.ts
+++ nextjs/src/handlers/logout.ts
@@ -5,12 +5,7 @@ import * as config from '../lib/config'
 
 const handleLogout = async (req: NextApiRequest, res: NextApiResponse) => {
     req.session.destroy()
-
-    // TODO - look for URL to goto after log out
-
-console.log('Logout called')
-
-    res.redirect('/')
+    res.redirect(config.defaultLoggedOutRoute) 
 }
 // wrap handler
 export default withIronSessionApiRoute( handleLogout, config.sessionOptions)
