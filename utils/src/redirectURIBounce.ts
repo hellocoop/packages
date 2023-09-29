8cf4cd69edec4e94bf7d33bcc38d1f8460eafd67
Dick Hardt <dick.hardt@gmail.com>
2023-09-28 18:00:11 -0700

    fix redirect_uri to only be the path with no query parameters

--- utils/src/redirectURIBounce.ts
+++ utils/src/redirectURIBounce.ts
@@ -5,7 +5,7 @@ export function redirectURIBounce() {
                 <script>
                     const baseURL = window.location.href.split("?")[0]
                     const searchParams = new URLSearchParams(window.location.search)
-                    searchParams.set("redirect_uri", window.location.href)
+                    searchParams.set("redirect_uri", window.location.origin + window.location.pathname)
                     window.location.href = baseURL + '?' + searchParams.toString()
                 </script>
             </head>
