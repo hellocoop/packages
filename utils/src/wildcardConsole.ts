f087d4eb0f80fffacf7c1cec027cf2b3935c2c7e
Dick Hardt <dick.hardt@hello.coop>
2023-09-29 10:41:45 -0700

    wildcard share

---
+++ utils/src/wildcardConsole.ts
@@ -0,0 +1,9 @@
+export function wildcardConsole( uri, target_uri, name, redirect_uri) {
+    return `
+        <html>
+            <head>
+                TBD
+            </head>
+        </html>
+    `
+}
