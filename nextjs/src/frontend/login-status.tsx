2863d3e70b67df35f8a68e85614e993902c1cfda
rohanharikr <rohanharikumar80@gmail.com>
2023-09-29 16:29:57 +0530

    fix: login status

--- nextjs/src/frontend/login-status.tsx
+++ nextjs/src/frontend/login-status.tsx
@@ -4,15 +4,14 @@ import useUser from "./user"
 
 export function LoggedIn({ children }: any) { //TBD type: any
     const user = useUser()
-    if(!user?.isLoggedIn)
+    if(!user || !user?.isLoggedIn)
         return
     return children
 }
 
 export function LoggedOut({ children }: any) { //TBD type: any
     const user = useUser()
-    if(user?.isLoggedIn)
+    if(!user || user?.isLoggedIn)
         return
-    
     return children
 }
