983b6f28c445b2bedaa2067783c15b4ca8cc3c6c
Dick Hardt <dick.hardt@hello.coop>
2023-09-29 10:44:22 -0700

    add in interface

--- utils/src/wildcardConsole.ts
+++ utils/src/wildcardConsole.ts
@@ -1,4 +1,10 @@
-export function wildcardConsole( uri, target_uri, name, redirect_uri) {
+export interface WildCard {
+    uri: string,
+    targetURI: string,
+    appName: string,
+    redirectURI: string
+}
+export function wildcardConsole( { uri, targetURI, appName, redirectURI}: WildCard ) {
     return `
         <html>
             <head>
