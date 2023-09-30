fbaafd0700410c77d9aabc3a442cb2132023a591
Dick Hardt <dick.hardt@gmail.com>
2023-09-30 02:34:02 -0700

    hack to test out writing to .env

--- quickstart-nextjs/index.js
+++ quickstart-nextjs/index.js
@@ -16,38 +16,40 @@ const qs = async function () {
     
     console.log('Hellō Quickstart for Next.js ... ')
 
-    const nextConfigFile = process.cwd()+'/next.config.js'
+    // const nextConfigFile = process.cwd()+'/next.config.js'
 
-    // check if we are in a directory with next.config.js
-    try {
-        fs.accessSync(nextConfigFile,fs.constants.F_OK)
-    } catch(err){
-        const error = new Error('Could not find "next.config.js". Are we running in a Next.js project?')
-        console.error(error)
-        return error    
-    }
+// FIX DETECTING IF IN NEXT.js 
+
+    // // check if we are in a directory with next.config.js
+    // try {
+    //     fs.accessSync(nextConfigFile,fs.constants.F_OK)
+    // } catch(err){
+    //     const error = new Error('Could not find "next.config.js". Are we running in a Next.js project?')
+    //     console.error(error)
+    //     return error    
+    // }
 
     // make sure we can read and write
-    try {
-        fs.accessSync(nextConfigFile,fs.constants.R_OK | fs.constants.W_OK)
-    } catch(err){
-        const error = new Error('Do not have read and write access to "next.config.js".')
-        console.error(error)
-        return error    
-    }
+    // try {
+    //     fs.accessSync(nextConfigFile,fs.constants.R_OK | fs.constants.W_OK)
+    // } catch(err){
+    //     const error = new Error('Do not have read and write access to "next.config.js".')
+    //     console.error(error)
+    //     return error    
+    // }
 
-    // check that the values have not already been set
-    try {
-        const nextConfig = await import(nextConfigFile)
-        const client_id = nextConfig?.default?.env?.HELLO_CLIENT_ID_DEFAULT
-        if (client_id) {
-            console.log(`HELLO_CLIENT_ID_DEFAULT already configured as ${client_id}`)
-            return
-        }
-    } catch(err) {
-        console.error('Error importing the package:', error);
-        return error 
-    }
+    // // check that the values have not already been set
+    // try {
+    //     const nextConfig = await import(nextConfigFile)
+    //     const client_id = nextConfig?.default?.env?.HELLO_CLIENT_ID_DEFAULT
+    //     if (client_id) {
+    //         console.log(`HELLO_CLIENT_ID_DEFAULT already configured as ${client_id}`)
+    //         return
+    //     }
+    // } catch(err) {
+    //     console.error('Error importing the package:', error);
+    //     return error 
+    // }
 
     let client_id = null
     try {
@@ -62,16 +64,25 @@ const qs = async function () {
     }
 
     const session_secret = randomBytes(32).toString('hex')
-    const helloConfig = `
-// added by @hellocoop/quickstart-nextjs
-nextConfig.env = {
-    ...nextConfig.env,
-    HELLO_CLIENT_ID_DEFAULT: '${client_id}',
-    HELLO_SESSION_SECRET_DEFAULT: '${session_secret}'
-}
-    `
+
+//     const helloConfig = `
+// // added by @hellocoop/quickstart-nextjs
+// nextConfig.env = {
+//     ...nextConfig.env,
+//     HELLO_CLIENT_ID_DEFAULT: '${client_id}',
+//     HELLO_SESSION_SECRET_DEFAULT: '${session_secret}'
+// }
+
+const helloConfig = `
+# added by @hellocoop/quickstart-nextjs
+HELLO_CLIENT_ID_DEFAULT='${client_id}'
+HELLO_SESSION_SECRET_DEFAULT='${session_secret}'
+`
+
+    const envFile = process.cwd()+'/.env'
+    
     try {
-        const err = fs.appendFileSync(nextConfigFile,helloConfig)
+        const err = fs.appendFileSync(envFile,helloConfig)
     } catch(err) {
         console.err(err)
         return err
