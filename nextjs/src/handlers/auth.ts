b3596a02cc8455302b59200f14093ae5022163f1
Dick Hardt <dick.hardt@hello.coop>
2023-10-03 11:42:13 -0700

    move config checking into API call

--- nextjs/src/handlers/auth.ts
+++ nextjs/src/handlers/auth.ts
@@ -17,16 +17,7 @@ const translateHandlerErrors = (handler: NextApiHandler): NextApiHandler =>
         }
     }
 
-let configured = true
-if (!config.sessionOptions.password) {
-    console.error('Missing HELLO_SESSION_SECRET configuration')
-    configured = false
-}
-
-if (!config.clientId) {
-    console.error('Missing HELLO_CLIENT_ID configuration')
-    configured = false
-}        
+      
 
 // console.log('config\n',JSON.stringify(config,null,4))   
 
@@ -34,7 +25,16 @@ export const handleAuth = translateHandlerErrors((req: NextApiRequest, res: Next
         const { query } = req
 
 console.log({query})     
+        let configured = true
+        if (!config.sessionOptions.password) {
+            console.error('Missing HELLO_SESSION_SECRET configuration')
+            configured = false
+        }
 
+        if (!config.clientId) {
+            console.error('Missing HELLO_CLIENT_ID configuration')
+            configured = false
+        }  
         if (query.profile) {
             if (configured)
                 return handleUser(req, res) 
