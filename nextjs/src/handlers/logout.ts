2fb994817857b43576023aad9b5d1c3dc38349d3
Dick Hardt <dick.hardt@gmail.com>
2023-10-08 07:35:08 -0700

    add in deleting auth

--- nextjs/src/handlers/logout.ts
+++ nextjs/src/handlers/logout.ts
@@ -1,10 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-
+import { clearAuthCookie } from '../lib/auth'
 import config from '../lib/config'
 
 const handleLogout = async (req: NextApiRequest, res: NextApiResponse) => {
     const { target_uri } = req.query
-    // TODO delete cookies
+    await clearAuthCookie(res)
     res.redirect(target_uri as string || config.routes.loggedOut) 
 }
 
