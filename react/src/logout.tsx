ace7692410e08ab34412449dbd3b9ccd5b6411ea
Dick Hardt <dick.hardt@hello.coop>
2023-10-31 12:27:24 -0700

    fix window reference

--- react/src/logout.tsx
+++ react/src/logout.tsx
@@ -3,5 +3,5 @@ import { routeConfig } from './provider'
 export const getLogOutRoute = () => routeConfig.logout
 
 export function logOut() {
-    window.location.href = routeConfig.logout
+    if (typeof window !== 'undefined') window.location.href = routeConfig.logout
 }
