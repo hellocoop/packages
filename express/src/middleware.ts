eda3bd91a932be4438667076ef936889aace02fa
Dick Hardt <dick.hardt@hello.coop>
2023-11-02 14:59:15 -0700

    fix link

--- express/src/middleware.ts
+++ express/src/middleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
-import { Auth } from 'lib/auth'
-import config from 'lib/config'
+import { Auth } from './lib/auth'
+import config from './lib/config'
 
 export const redirect = function ( target:string ) {
     return async ( req: Request, res: Response, next: NextFunction) => {
@@ -20,7 +20,7 @@ export const unauthorized = async ( req: Request, res: Response, next: NextFunct
         res.setHeader('WWW-Authenticate:',`Hello ${config.clientId}`).status(401)
 }
 
-export const auth = async ( req: Request, res: Response, next: NextFunction) => {
+export const setAuth = async ( req: Request, res: Response, next: NextFunction) => {
     await req.getAuth() // sets req.auth
     next()
 }
