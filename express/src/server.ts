b34fb31d8407f670c5da03dc24cb6f6f60b54a4c
Dick Hardt <dick.hardt@hello.coop>
2023-11-02 14:59:32 -0700

    add middleware samples

--- express/src/server.ts
+++ express/src/server.ts
@@ -1,15 +1,29 @@
 // test server
 import express, { Request, Response } from 'express'
 import auth from './index'
+import { redirect, unauthorized, setAuth } from './middleware'
+
 const app = express();
 const port = 8080; // default port to listen
 
 app.use(auth({client_id:'90804992-8d01-474e-8a0c-59cddeb5a1a3'}))
 
-// define a route handler for the default home page
-app.get( "/", ( req: Request, res: Response ) => {
-    res.send( "Hello world!" );
+app.get( "/", async ( req: Request, res: Response ) => {
+    res.send( JSON.stringify(await req.getAuth()) );
 } );
+
+app.get( "/redirect", redirect('/'), async ( req: Request, res: Response ) => {
+    res.send( JSON.stringify(await req.getAuth()) );
+} );
+
+app.get( "/unauthorized", unauthorized, async ( req: Request, res: Response ) => {
+    res.send( JSON.stringify(await req.getAuth()) );
+} );
+
+app.get( "/setAuth", setAuth, ( req: Request, res: Response ) => {
+    res.send( JSON.stringify(req.auth) );
+} );
+
 
 // start the Express server
 app.listen( port, () => {
