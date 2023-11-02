b01eca150b164f65b1392ac782fc73d82f00c6a1
Dick Hardt <dick.hardt@gmail.com>
2023-11-02 15:27:10 -0700

    send json

--- express/src/server.ts
+++ express/src/server.ts
@@ -9,19 +9,19 @@ const port = 8080; // default port to listen
 app.use(auth({client_id:'90804992-8d01-474e-8a0c-59cddeb5a1a3'}))
 
 app.get( "/", async ( req: Request, res: Response ) => {
-    res.send( JSON.stringify(await req.getAuth()) );
+    res.json( await req.getAuth()) 
 } );
 
 app.get( "/redirect", redirect('/'), async ( req: Request, res: Response ) => {
-    res.send( JSON.stringify(await req.getAuth()) );
+    res.json( await req.getAuth()) 
 } );
 
 app.get( "/unauthorized", unauthorized, async ( req: Request, res: Response ) => {
-    res.send( JSON.stringify(await req.getAuth()) );
+    res.json( await req.getAuth()) 
 } );
 
 app.get( "/setAuth", setAuth, ( req: Request, res: Response ) => {
-    res.send( JSON.stringify(req.auth) );
+    res.json( req.auth) 
 } );
 
 
