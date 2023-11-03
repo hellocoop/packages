d34c9c0007ec46f237746ecbaa241dbf5e5880ce
rohanharikr <rohanharikumar80@gmail.com>
2023-11-03 15:06:06 +0530

    remove test projects, update vue readme

--- svelte-test/src/App.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import { HelloProvider } from '@hellocoop/svelte'
-  import Counter from './lib/Counter.svelte';
-</script>
-
-
-<HelloProvider config={{login: "/api/hello-world"}}>
-  <Counter/>
-</HelloProvider>
--- svelte-test/src/lib/Counter.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import { LoginButton, getHelloProviderContext } from "@hellocoop/svelte";
-    const test = getHelloProviderContext()
-</script>
-
-<LoginButton/>
-
-{JSON.stringify(test)}
