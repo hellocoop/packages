d6be6191d28a21b31fd54c19de8c7d6c54c55e83
Dick Hardt <dick.hardt@gmail.com>
2023-11-09 07:44:26 -0800

    remove comments

--- fastify/src/auth.ts
+++ fastify/src/auth.ts
@@ -6,13 +6,8 @@ import {
 import fp from 'fastify-plugin'
 
 import { 
-    FastifyInstance,
-    FastifyRegisterOptions,
-    FastifyPluginCallback,
-    // fastify next function 
     FastifyRequest,
     FastifyReply,
-    RouteHandler,
     FastifyPluginOptions
 } from 'fastify'
 
@@ -29,23 +24,6 @@ import {
     configure,
     Config,
 }  from '@hellocoop/router'
-
-
-
-// export type AuthRequest = Request & {
-//     auth?: Auth
-// }
-
-
-// const clearAuthCookie = ( res: HelloResponse) => {
-//     const { name, value, options } = clearAuthCookieParams()
-//     res.setCookie(name, value, options)
-// }
-
-// const sendJSON = (res: FastifyReply, data: any) => {
-//     res.header('Content-Type', 'application/json')
-//     res.send(JSON.stringify(data))
-// }
 
 const convertToHelloRequest = ( req: FastifyRequest ): HelloRequest => {
     return {
@@ -81,22 +59,6 @@ const convertToHelloResponse = ( res: FastifyReply ): HelloResponse => {
 }
 
 
-// const setAuthMiddleware = async function ( req: FastifyRequest, res: FastifyReply, next: NextFunction) {
-//     req.getAuth = async (): Promise<Auth> => {
-//         const helloReq = convertToHelloRequest(req)
-//         const helloRes = convertToHelloResponse(res)
-//         if (req.auth)
-//             return req.auth
-//         req.auth = await getAuthfromCookies( helloReq, helloRes) || NotLoggedIn
-//         return req.auth as Auth
-//     }
-//     res.clearAuth = () => {
-//         const helloRes = convertToHelloResponse(res)
-//         clearAuthCookie(helloRes)
-//     }
-//     next()
-// }
-
 
 declare module 'fastify' {
     interface FastifyRequest {
@@ -112,7 +74,8 @@ export interface HelloConfig extends FastifyPluginOptions, Config {}
   
   
   const helloPlugin: FastifyPluginAsync <HelloConfig> = async (instance, options) => {
-    configure(options)
+    if (!isConfigured)
+       configure(options)
     instance.decorateRequest('auth', undefined)
     instance.decorateRequest('getAuth', async function () { 
         const helloReq = convertToHelloRequest(this)
@@ -130,11 +93,5 @@ export interface HelloConfig extends FastifyPluginOptions, Config {}
       return await router(helloReq, helloRes)
     })
   }
-  
-  // export plugin using fastify-plugin
-  
-
-//   export default fp( helloPlugin, '3.x')
-
 
 export default fp( helloPlugin )
