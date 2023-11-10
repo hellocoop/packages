0773a763759df075ddcf812adfb57c138eaf6ca5
Dick Hardt <dick.hardt@gmail.com>
2023-11-10 14:44:54 -0800

    change provider signature to avoid confusion

--- react/src/provider.tsx
+++ react/src/provider.tsx
@@ -14,12 +14,12 @@ export const routeConfig: RouteConfig = {
 
 const HelloContext = createContext(undefined)
 
-const HelloProvider = ({ children, auth, config} : any) => { //TBD any
+const HelloProvider = ({ children, status, config} : any) => { //TBD any
     if (config?.login) routeConfig.login = config.login 
     if (config?.auth) routeConfig.auth = config.auth 
     if (config?.logout) routeConfig.logout = config.logout 
     return (
-      <HelloContext.Provider value={auth}>
+      <HelloContext.Provider value={status}>
           {children}
       </HelloContext.Provider>
     )
