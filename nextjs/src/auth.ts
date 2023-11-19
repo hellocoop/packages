6d68d83a83b106eed7212f1baabf087a45d105dd
Dick Hardt <dick.hardt@gmail.com>
2023-11-19 14:15:44 -0800

    remove logging

--- nextjs/src/auth.ts
+++ nextjs/src/auth.ts
@@ -5,18 +5,15 @@ import { cookies } from 'next/headers'
 import { decryptObj } from '@hellocoop/core'
 import { NotLoggedIn} from '@hellocoop/constants'
 
+// TODO cache decryption
+// import { unstable_cache } from 'next/cache';
+// https://nextjs.org/docs/app/api-reference/functions/unstable_cache
+
 export const auth = async function (): Promise<Auth> {
-
     const authCookie = cookies().get(configuration.cookies.authName)?.value
-
-console.log({authCookie})
-
     if (!authCookie)
         return NotLoggedIn
     const a = await decryptObj(authCookie, configuration.secret as string) as Auth
-
-console.log({a})
-
     if (!a)
         return NotLoggedIn
     return a 
