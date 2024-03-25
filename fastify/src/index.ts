80a0cca352d12a34f5c2322f802197bebab1ddec
Dick Hardt <dick.hardt@hello.coop>
2024-03-24 18:02:06 -0700

    export LoginTrigger*

--- fastify/src/index.ts
+++ fastify/src/index.ts
@@ -1,4 +1,5 @@
 import { auth, HelloConfig } from './auth'
 export default auth
 export { auth as helloAuth, HelloConfig }
+export { LoginTriggerParams, LoginTriggerResponse } from '@hellocoop/router'
 export { redirect, unauthorized, setAuth } from './middleware'
