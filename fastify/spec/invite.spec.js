8a0e09bef56f86c192865f540e5ef0b9ab7cff41
rohanharikr <rohanharikumar80@gmail.com>
2024-10-18 17:26:37 +0100

    comment out code

--- fastify/spec/invite.spec.js
+++ fastify/spec/invite.spec.js
@@ -1,6 +1,30 @@
-// import fastify from './fastify'
+// const { expect } = require('chai')
+// const Fastify = require('./fastify')
 
 // describe('Invite', async () => {
-//     it('Call ?op=invite', async () => {})
-//     it('Redirect to invite app with correct invite params', async () => {})
+//     let fastify = null
+
+//     before( async () => {
+//         fastify = await Fastify().ready()
+//     })
+
+//     it('Login', async () => {
+//         const response = await fastify.inject({
+//             method: 'GET',
+//             url: '/api/hellocoop?op=login'
+//         })
+//         expect(response.statusCode).to.eql(200)
+//         console.log(response.body)
+//     })
+
+    // it('Call ?op=invite', async () => {
+    //     const response = await fastify.inject({
+    //         method: 'GET',
+    //         url: '/api/hellocoop?op=invite&app_name=Test'
+    //     })
+    //     expect(response.statusCode).to.eql(200)
+    // })
+
+    
+    // it('Redirect to authorize app with correct params', async () => {})
 // })
--- fastify/spec/provider-initiated-login.spec.js
+++ fastify/spec/provider-initiated-login.spec.js
@@ -1,21 +1,21 @@
-const { expect } = require('chai')
-const Fastify = require('./fastify')
+// const { expect } = require('chai')
+// const Fastify = require('./fastify')
 
-describe('Operations', async () => {
-    let fastify = null
+// describe('Operations', async () => {
+//     let fastify = null
 
-    before( async () => {
-        fastify = await Fastify().ready()
-    })
+//     before( async () => {
+//         fastify = await Fastify().ready()
+//     })
 
-    it('Load /api/hellocoop with iss param', async () => {
-        const response = await fastify.inject({
-            method: 'GET',
-            url: '/api/hellocoop?iss=https://issuer.hello.coop&login_hint=mailto:johnsmith@me.com'
-        })
-        expect(response.statusCode).to.eql(200)
-    })
+//     it('Load /api/hellocoop with iss param', async () => {
+//         const response = await fastify.inject({
+//             method: 'GET',
+//             url: '/api/hellocoop?iss=https://issuer.hello.coop&login_hint=mailto:johnsmith@me.com'
+//         })
+//         expect(response.statusCode).to.eql(200)
+//     })
 
     
-    it('Redirect to authorize app with correct params', async () => {})
-})
+//     it('Redirect to authorize app with correct params', async () => {})
+// })
